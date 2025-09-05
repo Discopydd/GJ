2453a7d84627cb6b4baad1afa367d8ce09c06ee5
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-09-05 10:33:59 +0900

    kfh

--- DirectXGame/Border.cpp
+++ DirectXGame/Border.cpp
@@ -1,1 +1,19 @@
 #include "Border.h"
+#include<cassert>
+
+Border::~Border() {}
+
+void Border::Initialize(KamataEngine::Vector3 pos, KamataEngine::Model* model) { 
+	assert(model); 
+	model_ = model;
+	color_.Initialize();
+	worldTransform_.Initialize();
+	worldTransform_.translation_ = pos;
+}
+
+void Border::Update() { 
+	
+	worldTransform_.TransferMatrix(); }
+
+void Border::Draw(KamataEngine::Camera* camera) { model_->Draw(worldTransform_, *camera,&color_); }
+
--- DirectXGame/Border.h
+++ DirectXGame/Border.h
@@ -1,2 +1,18 @@
 #pragma once
-class Border {};
+#include"KamataEngine.h"
+class Border {
+public:
+	~Border();
+	void Initialize(KamataEngine::Vector3,KamataEngine::Model*);
+	void Update();
+	void Draw(KamataEngine::Camera*);
+	//void OnCollision();
+	bool GetFlag() { return flag_; }
+
+private:
+	KamataEngine::WorldTransform worldTransform_;      
+	KamataEngine::Model* model_ = nullptr;
+	KamataEngine::ObjectColor color_;
+	bool flag_ = false;
+};
+
